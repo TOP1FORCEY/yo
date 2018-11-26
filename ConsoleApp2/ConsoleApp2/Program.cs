50cfbc342b83047840644446a5bd2ff614fcc6b8
bomj <romajen2114@gmail.com>
2018-11-26 17:37:58 +0200

    2

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static string a;
+        static int hohoh = 1;
+        static void Main(string[] args)
+        {
+            hohoh = Convert.ToInt32(Console.ReadLine());
+            hohoh = hohoh * hohoh;
+            Console.WriteLine("UR S IS");
+            Console.WriteLine(hohoh);
+
+            Console.ReadKey();
+        }
+    }
+}
